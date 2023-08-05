c77f3fd2d6f58d460ea577d8378d330ab9e91a8e
jfioasd <120231014+jfioasd@users.noreply.github.com>
2023-08-05 10:14:45 +0800

    Update stack.c

--- stack.c
+++ stack.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+void run(char *prog, int *sp) {
+    printf("%p %d", sp, *sp);
+}
+
+int main(int argc, char **argv) {
     int *stack = (int*)malloc(20000 * sizeof(int));
     int *sp = stack;
 
-    printf("%d\n", *sp);
-    *sp = 22; 
-    printf("%d %p\n", *sp, sp);
-    sp ++; 
-    printf("%d %p\n", *sp, sp);
-    sp --; 
-    printf("%d %p\n", *sp, sp);
+    FILE *fp;
+    char prog[2000];
 
-    printf("%p", stack);
+    fp = fopen(argv[1], "r");
+
+    fread(&prog, sizeof(char), -1, fp);
+
+    run(prog, sp);
 
     free(stack);
+    return 0;
 }
